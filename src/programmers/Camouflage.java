6cdc8dc75dc815187659d4ac89f45b8c41962145
JisooJang <ppuagirls@naver.com>
2020-05-07 02:16:09 +0900

    refactor: refactor Camouflage class in programmers package

--- src/programmers/Camouflage.java
+++ src/programmers/Camouflage.java
@@ -1,37 +1,29 @@
 package programmers;
 
 import java.util.HashMap;
-import java.util.Map;
+import java.util.Iterator;
 
 /*
 https://programmers.co.kr/learn/courses/30/lessons/42578
 스파이가 가진 의상들이 담긴 2차원 배열 clothes가 주어질 때
 서로 다른 옷의 조합의 수를 return 하도록 solution 함수를 작성해주세요.
  */
-public class Camouflage {
+
+class Camouflage {
     public int solution(String[][] clothes) {
-        int total_count = clothes.length;
-
-        Map<String, Integer> category = new HashMap<String, Integer>();
-
-        for(int i=0 ; i<clothes.length ; i++) {
-            if(category.containsKey(clothes[i][1])) {
-                category.put(clothes[i][1], category.get(clothes[i][1]) + 1);
+        int answer = 1;
+        HashMap<String, Integer> map = new HashMap<>();
+        for (String[] clothe : clothes) {
+            String key = clothe[1];
+            if (!map.containsKey(key)) {
+                map.put(key, 1);
             } else {
-                category.put(clothes[i][1], 1);
+                map.put(key, map.get(key) + 1);
             }
-            //category.put(clothes[i][1], category.getOrDefault(clothes[i][1], 0) + 1);
         }
-
-        if(category.keySet().size() == 1) {
-            return total_count;
+        for (Integer integer : map.values()) {
+            answer *= integer + 1;
         }
-
-        int sum = 1;
-        for(int count : category.values()) {
-            sum *= count;
-        }
-
-        return total_count + sum;
+        return answer-1;
     }
 }
