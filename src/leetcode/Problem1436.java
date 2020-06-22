e35fab1f90857cafab3eb56a363e5edfd695b9b7
JisooJang <ppuagirls@naver.com>
2020-06-22 22:23:10 +0900

    refactor: add destCity2 method for refactoring Problem1436 in leetcode

--- src/leetcode/Problem1436.java
+++ src/leetcode/Problem1436.java
@@ -1,8 +1,6 @@
 package leetcode;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 /*
 https://leetcode.com/problems/destination-city/
@@ -19,6 +17,7 @@ public class Problem1436 {
     }
 
     public static String destCity(List<List<String>> paths) {
+        // O(n^)
         boolean chk;
         // 풀이 : 각 리스트의 마지막 원소를 기준으로, 다른 리스트들의 첫번째 원소가 마지막 원소와 같은 값이 존재하면 마지막 destincation 값이 아님.
         // 즉 다른 리스트들의 첫번쨰 원소가 기준 리스트의 마지막 원소랑 겹치는 값이 없으면 destination
@@ -35,6 +34,38 @@ public class Problem1436 {
             }
             if(!chk) return lastElement;
         }
+        return null;
+    }
+
+    public static String destCity2(List<List<String>> path) {
+        Map<String, Integer> countOfElements = new HashMap<>();
+
+        // O(N)
+        for (List<String> strings : path) {
+            String element1 = strings.get(0);
+            String element2 = strings.get(1);
+            countOfElements.put(element1, countOfElements.getOrDefault(element1, 0) + 1);
+            countOfElements.put(element2, countOfElements.getOrDefault(element2, 0) + 1);
+        }
+
+        // O(N)
+        String[] candidates = new String[2];
+        int i=0;
+        for(Map.Entry<String, Integer> entry : countOfElements.entrySet()) {
+            // start, dest가 아닌 원소들은 value들이 2로 저장되어있다. start, dest는 짝지어진 다른 원소가 없으므로 value가 1이다.
+            if(entry.getValue() == 1) {
+                candidates[i++] = entry.getKey();
+            }
+        }
+
+        // start, dest가 저장되어있는 candidates 중 (인덱스가 1인 아이를 return) == dest
+        for(List<String> strings : path) {
+            int index1 = strings.indexOf(candidates[0]);
+            int index2 = strings.indexOf(candidates[1]);
+            if(index1 == 1) return candidates[0];
+            if(index2 == 1) return candidates[1];
+        }
+
         return null;
     }
 }
