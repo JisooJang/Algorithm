e4213b7dc9d85ad717c90d969748316ec0c0d773
JisooJang <ppuagirls@naver.com>
2020-07-01 12:25:20 +0900

    feat : add sum method in RecursionTest class in common package

--- src/common/RecursionTest.java
+++ src/common/RecursionTest.java
@@ -3,6 +3,7 @@ package common;
 public class RecursionTest {
     public static void main(String[] args) {
         printInBinary(24);
+        System.out.println(sum(8, new int[]{1, 2, 3, 4, 5, 6, 7, 8}));
     }
     public static void printInBinary(int n) {
         if(n < 2) {
@@ -10,6 +11,15 @@ public class RecursionTest {
         } else {
             printInBinary(n / 2); // 12 -> 6 -> 3
             System.out.print(n % 2); //  1 -> 0 -> 0 -> 0
+        }
+        System.out.println();
+    }
+
+    public static int sum(int n, int[] data) {
+        if(n == 0) {
+            return 0;
+        } else {
+            return data[n-1] + sum(n-1, data);
         }
     }
 }
