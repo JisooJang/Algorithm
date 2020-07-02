4d714649f13a190795246a7068b730035f7250fa
JisooJang <ppuagirls@naver.com>
2020-07-02 12:34:08 +0900

    feat : add more recursion function in RecursionTest class

--- src/common/RecursionTest.java
+++ src/common/RecursionTest.java
@@ -4,6 +4,10 @@ public class RecursionTest {
     public static void main(String[] args) {
         printInBinary(24);
         System.out.println(sum(8, new int[]{1, 2, 3, 4, 5, 6, 7, 8}));
+        System.out.println(findMax(new int[]{1, 2, 3, 4, 5, 6, 7, 8}, 0, 7));
+        System.out.println(findMax2(new int[]{1, 2, 3, 4, 5, 6, 7, 8}, 0, 7));
+        System.out.println(binarySearch(new String[]{"aaa", "bbb", "ccc", "ddd", "eee"}, "ffff", 0, 4));
+        System.out.println(binarySearch(new String[]{"aaa", "bbb", "ccc", "ddd", "eee"}, "11", 0, 4));
     }
     public static void printInBinary(int n) {
         if(n < 2) {
@@ -20,6 +24,46 @@ public class RecursionTest {
             return 0;
         } else {
             return data[n-1] + sum(n-1, data);
+        }
+    }
+
+    public static Integer findMax(int[] arr, int beginIndex, int endIndex) {
+        if(beginIndex == endIndex) {
+            return arr[beginIndex];
+        }
+        System.out.println(arr[beginIndex]);
+        int result = Math.max(arr[beginIndex], findMax(arr, beginIndex + 1, endIndex));
+        System.out.println("result : " + arr[beginIndex] + ":" + result);
+        return result;
+    }
+
+    public static Integer findMax2(int[] arr, int beginIndex, int endIndex) {
+        if(beginIndex == endIndex) {
+            return arr[beginIndex];
+        }
+        int mid = (beginIndex + endIndex) / 2;
+        System.out.println("mid : " + mid);
+
+        int leftMax = findMax2(arr, beginIndex, mid);
+        System.out.println("leftMax : " + leftMax);
+
+        int rightMax = findMax2(arr, mid + 1, endIndex);
+        System.out.println("rightMax : " + rightMax);
+
+        return Math.max(leftMax, rightMax);
+    }
+
+    public static int binarySearch(String[] arr, String target, int beginIndex, int endIndex) {
+        if(beginIndex >= endIndex) {
+            return -1;
+        }
+        int mid = (beginIndex + endIndex) / 2;
+        int result = target.compareTo(arr[mid]);
+        if(result == 0) return mid;
+        else if(result < 0) {
+            return binarySearch(arr, target, beginIndex, mid - 1);
+        } else {
+            return binarySearch(arr, target, mid + 1, endIndex);
         }
     }
 }
